566ab7aecd19ca79691c16ee961c075a73470ae3
frebern <frebern@naver.com>
2022-06-15 05:02:57 +0900

    fix: alias 변경으로 빌드 깨지는 문제 해결

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import reportWebVitals from './reportWebVitals'
 
 import '@fontsource/roboto'
 import '@fontsource/noto-sans'
-import '@/index.css'
-import App from '@/App'
+import './index.css'
+import App from './App'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
