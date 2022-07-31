f2fee641db7d411e8705d9e53fa1862a2a0cfb8d
frebern <frebern@naver.com>
2022-07-31 15:31:16 +0900

    feat: PWA 설치가 가능하다면 사용자에게 한번 물어보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Paper, ThemeProvider } from '@mui/material'
+import { usePWAInstall } from 'react-use-pwa-install'
+import { useEffect } from 'react'
 import Router from './Router'
 import theme from './theme'
 
 const queryClient = new QueryClient()
 
 function App () {
+  /* PWA 설치 가능하다면 처음 한번 Install 물어보기 */
+  const install = usePWAInstall()
+  const isInstallReady = install !== null
+
+  useEffect(() => {
+    if (!isInstallReady) return
+    install?.()
+  }, [isInstallReady])
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
--- src/core/firestore.ts
+++ src/core/firestore.ts
@@ -1,13 +1,7 @@
 import { initializeApp } from 'firebase/app'
-// import { getAnalytics } from 'firebase/analytics'
+import { getAnalytics } from 'firebase/analytics'
 import { getFirestore, QueryDocumentSnapshot, SnapshotOptions } from 'firebase/firestore'
 
-/**
- * NOTE:
- * firebase의 API Key는 적절한 조치를 취해두면 공개되어도 괜찮습니다.
- * 이 프로젝트에서는 HTTP 레퍼러 제한을 걸어둡니다.
- * @see https://firebase.google.com/docs/projects/api-keys#apply-restrictions
- */
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FB_API_KEY,
   authDomain: 'frebern-yellow-room.firebaseapp.com',
@@ -19,7 +13,7 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-// export const analytics = getAnalytics(app)
+export const analytics = getAnalytics(app)
 export default getFirestore(app)
 
 /** Firestore 문서와 코드쪽 도메인 모델을 변환 */
