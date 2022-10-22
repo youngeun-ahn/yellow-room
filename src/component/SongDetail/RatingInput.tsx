2f4943d9a3050e411cf4ea226fd7fbc3c6a444f4
frebern <frebern@naver.com>
2022-10-22 16:19:51 +0900

    fix: Rating 조정 버그 수정

--- src/component/SongDetail/RatingInput.tsx
+++ src/component/SongDetail/RatingInput.tsx
@@ -1,20 +1,18 @@
 import { Rating, RatingProps, useEventCallback } from '@mui/material'
-import { forwardRef, Ref, useState } from 'react'
+import { forwardRef, Ref } from 'react'
 
 function RatingInput (
   { value, onChange, ...props }: RatingProps,
   ref: Ref<unknown>,
 ) {
-  const [startRating, setStartRating] = useState(value ?? 0)
-
   const onTouchMove: RatingProps['onTouchMove'] = useEventCallback(e => {
     const left = e.currentTarget.clientLeft
     const width = e.currentTarget.clientWidth
     const unit = width / 5
 
     const diff = e.touches[0].clientX - left
-    const diffRating = Math.round((diff / unit) * 2) / 2 - 0.5
-    onChange?.(e, startRating + diffRating)
+    const rating = Math.round((diff / unit) * 2) / 2 - 0.5
+    onChange?.(e, rating)
   })
 
   return (
@@ -23,7 +21,6 @@ function RatingInput (
       precision={0.5}
       ref={ref}
       value={value}
-      onTouchStart={() => setStartRating(value ?? 0)}
       onTouchMove={onTouchMove}
       onChange={onChange}
       {...props}
